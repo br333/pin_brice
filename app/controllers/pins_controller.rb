7be7d1fee91bca1764fd3dadeaa506dd42f9e028
Brice Francisco <bricefrancisco112@gmail.com>
2015-06-28 17:50:02 +0800

    PINS AND NEEDLES

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,52 @@
+class PinsController < ApplicationController
+	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	def index
+		@pins = Pin.all.order("created_at ASC")
+	end
+
+	def show
+
+	end
+
+	def new
+		@pin= Pin.new
+	end
+
+	def create
+		@pin= Pin.new(pin_params)
+
+		if @pin.save
+			redirect_to @pin, notice: "Successfully created new Brice!"
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+
+	end
+
+	def update
+		if @pin.update(pin_params)
+			redirect_to @pin, notice: "Pin was successfully BRICE!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pin.destroy
+		redirect_to pins_path
+
+	end
+
+	private
+
+	def pin_params
+		params.require(:pin).permit(:title, :description)
+	end
+
+	def find_pin
+		@pin = Pin.find(params[:id])
+	end
+end
